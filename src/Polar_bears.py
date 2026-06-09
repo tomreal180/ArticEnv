9feae77365511c7203f143aa32079653ae99ceec
Minh Duc Ha <tomreal180@gmail.com>
2026-06-09 13:06:04 +0200

    New: automate polar bear and add mesh bear

--- src/Polar_bears.py
+++ src/Polar_bears.py
@@ -1,38 +1,80 @@
+from direct.interval.IntervalGlobal import Sequence
+
 from direct.actor.Actor import Actor
-from panda3d.core import CollisionNode, CollisionRay, CollisionHandlerFloor, BitMask32
+from panda3d.core import CollisionNode, CollisionRay, CollisionHandlerFloor, BitMask32, Point3
 class PolarBears(Actor):
     def __init__(self, render, config=None, cTrav=None):
         self.render = render
         if config is None:
             config = {}
-        actor_model = config.get("model_path", "assets/models/polar_bear_walk.glb")
+        self.mesh_bear = None
+        actor_model = config.get("model_path", "assets/models/polar_bear2.glb")
         super().__init__(actor_model, {"walk": actor_model})
         self.reparentTo(self.render)
         actor_scale = config.get("scale", 1.0)
         self.setScale(actor_scale, actor_scale, actor_scale)
+        self.create_mesh_bear()
         # lower panda slightly so it sits closer to the ground
         # adjust this value as needed
+        z = config.get("z_pos", 0)
         self.setZ(config.get("z_pos", 0))
+        self.setH(250)
+        # self.setHpr(0, 0, 0)  
         self.loop("walk")
 
-        self.setupCollision(cTrav)
-    def setupCollision(self, cTrav):
-        if cTrav is None:
-            return  # No collision traverser provided, skip setup
-         # Tạo tia Ray chiếu từ trên xuống (cao 100 đơn vị so với gốc của gấu)
-        ray = CollisionRay(0, 0, 100, 0, 0, -1)
-        rayNode = CollisionNode('bearRay')
-        rayNode.addSolid(ray)
-        # Mask 1 để chỉ va chạm với môi trường (mặt đất)
-        rayNode.setFromCollideMask(BitMask32.bit(1))
-        rayNode.setIntoCollideMask(BitMask32.allOff())
+        posInterval1 = self.posInterval(50, Point3(5.0796813, -0.5430069, z), startPos=Point3(112.55255, -39.659835, z+2))
+        posInterval2 = self.posInterval(50, Point3(112.55255, -39.659835, z+2), startPos=Point3(5.0796813, -0.5430069, z))
+        hprInterval1 = self.hprInterval(3, Point3(70, 0, 0), startHpr=Point3(250, 0, 0))
+        hprInterval2 = self.hprInterval(3, Point3(250, 0, 0), startHpr=Point3(70, 0, 0))
+        self.pandaPace = Sequence(posInterval1, hprInterval1, posInterval2, hprInterval2, name="pandaPace")
+        self.pandaPace.loop()
+
+        # self.setupCollision(cTrav)
+    # def setupCollision(self, cTrav):
+    #     if cTrav is None:
+    #         return  # No collision traverser provided, skip setup
+    #      # Tạo tia Ray chiếu từ trên xuống (cao 100 đơn vị so với gốc của gấu)
+    #     ray = CollisionRay(0, 0, 100, 0, 0, -1)
+    #     rayNode = CollisionNode('bearRay')
+    #     rayNode.addSolid(ray)
+    #     # Mask 1 để chỉ va chạm với môi trường (mặt đất)
+    #     rayNode.setFromCollideMask(BitMask32.bit(1))
+    #     rayNode.setIntoCollideMask(BitMask32.allOff())
         
-        self.rayNodePath = self.attachNewNode(rayNode)
-        self.floorHandler = CollisionHandlerFloor()
-        self.floorHandler.addCollider(self.rayNodePath, self)
+    #     self.rayNodePath = self.attachNewNode(rayNode)
+    #     self.floorHandler = CollisionHandlerFloor()
+    #     self.floorHandler.addCollider(self.rayNodePath, self)
         
-        # Đăng ký với hệ thống tính toán va chạm
-        cTrav.addCollider(self.rayNodePath, self.floorHandler)
+    #     # Đăng ký với hệ thống tính toán va chạm
+    #     cTrav.addCollider(self.rayNodePath, self.floorHandler)
+
+    def create_mesh_bear(self):
+        self.mesh_bear = self.render.attachNewNode("mesh_bear")
+        self.instanceTo(self.mesh_bear)
+
+        # 2. Ép bản sao hiển thị dưới dạng lưới (Wireframe)
+        self.mesh_bear.setRenderModeWireframe()
+        self.mesh_bear.setRenderModeThickness(2) # Độ dày của sợi lưới
+        
+        # 3. Đặt màu sắc và tắt các hiệu ứng che khuất
+        self.mesh_bear.setColor(1, 0, 0, 1, 1)   # Lưới màu Đỏ rực
+        self.mesh_bear.setTextureOff(1)          # Tắt ảnh da gấu
+        self.mesh_bear.setLightOff(1)            # Tắt bóng đổ
+
+        # 4. KỸ THUẬT QUAN TRỌNG: Đẩy lưới nổi lên trên để không bị nhấp nháy với da gấu gốc
+        self.mesh_bear.setDepthOffset(1)
+
+        # Mặc định ẩn lưới đi
+        self.mesh_bear.hide()
+
+    def toggle_outline(self):
+        """Hàm xử lý việc bật/tắt viền"""
+        if self.mesh_bear.isHidden():
+            self.mesh_bear.show()
+        else:
+            self.mesh_bear.hide()
+    
+
     
     def changePandaScale(self, factor):
         """Scale the panda actor by factor."""
