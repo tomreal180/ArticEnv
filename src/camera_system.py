f094f7a504831ecc9ac458e89cd22b1cf77f4a41
Minh Duc Ha <tomreal180@gmail.com>
2026-06-10 21:04:42 +0200

    Update:Save current camera position

--- src/camera_system.py
+++ src/camera_system.py
@@ -22,14 +22,17 @@ class CameraSystem:
         # self.camera.getDisplayRegion(0).setActive(0)  # Kích hoạt DisplayRegion của camera chính
         # Tắt các task cập nhật camera mỗi khung hình để camera cố định
         # self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+        self.cameraPosition = self.camerasystem["Cam_Main"]["camera"].getPos()
         self.zoom = False
         self.taskMgr.add(self.followCameraTask, "FollowCameraTask")
+        self.taskMgr.add(self.ListenCameraPosition, "ListenCameraPositionTask")
 
     def setTarget(self, target):
-        """Thiết lập mục tiêu để camera đi theo."""
+        """Set the target node for the camera to follow."""
         self.target = target
 
     def spinCameraTask(self, task):
+        """Spin the camera around the origin in a circular path."""
         angleDegrees = task.time * 6.0
         angleRadians = angleDegrees * (pi / 180.0)
         self.camera.setPos(self.cameraDistance * sin(angleRadians), -self.cameraDistance * cos(angleRadians), self.cameraHeight)
@@ -44,8 +47,8 @@ class CameraSystem:
     #         self.camerasystem[cam_name] = main_cam
 
     def create_cameras(self):
+        """Create multiple cameras for different views and assign them to specific viewports."""
         self.create_viewport_camera("Cam_Main", viewport=(0.0, 1.0, 0.0, 1.0))
-        # self.camera = self.camerasystem["Cam_Main"]["camera"]
         self.camerasystem["Cam_Main"]["viewport"].setActive(1)
         self.create_viewport_camera("Cam_TopDown", viewport=(0.0, 0.5, 0.5, 1.0))
         self.create_viewport_camera("Cam_Behind", viewport=(0.5, 1.0, 0.5, 1.0))
@@ -54,7 +57,7 @@ class CameraSystem:
 
     
     def create_viewport_camera(self, cam_name, viewport):
-        """Hàm khởi tạo camera và gán vào một khung (DisplayRegion) trên màn hình"""
+        """Function to create a camera and attach it to a viewport (DisplayRegion) on the screen"""
         
         l, r, b, t = viewport
 
@@ -78,7 +81,7 @@ class CameraSystem:
         # cam.lookAt(self.target.getPos())
 
         
-        # Lưu trữ lại để quản lý
+        # save camera and its viewport in the camerasystem dictionary for later reference
         self.camerasystem[cam_name] = {
             "name": cam_name,
             "camera": cam_np,
@@ -97,11 +100,10 @@ class CameraSystem:
                 if camera:
                     if cam_name == "Cam_TopDown":
                         camera.setPos(target_pos.getX(), target_pos.getY(), target_pos.getZ() + self.cameraDistance)
-                        # Nhìn thẳng xuống (Pitch = -90), hướng (Heading) xoay ngược lại gấu (+180) để mông hướng lên trên
                         camera.setHpr(heading + 180, -90, 0)
                     elif cam_name == "Cam_Behind":
                         angle_radians = heading * (pi / 180.0)
-                        # Tính toán vị trí camera nằm ở phía trước mặt nhân vật
+                        # calculate camera position behind the target based on its heading
                         cam_x = target_pos.getX() - self.cameraDistance * sin(angle_radians)
                         cam_y = target_pos.getY() + self.cameraDistance * cos(angle_radians)
                         camera.setPos(cam_x, cam_y, self.cameraHeight)
@@ -114,33 +116,56 @@ class CameraSystem:
                         camera.lookAt(target_pos.getX(), target_pos.getY(), target_pos.getZ())
                     elif cam_name == "Cam_Left":
                         angle_radians = (heading + 90) * (pi / 180.0)
-                        # Tính toán vị trí camera nằm ở phía trước mặt nhân vật
+                        # calculate camera position to the left of the target based on its heading
                         cam_x = target_pos.getX() - self.cameraDistance * sin(angle_radians)
                         cam_y = target_pos.getY() + self.cameraDistance * cos(angle_radians)
                         camera.setPos(cam_x, cam_y, self.cameraHeight)
                         camera.lookAt(target_pos.getX(), target_pos.getY(), target_pos.getZ()+1.5)
                     else:
-                        raise Exception(f"Camera {cam_name} chưa được định nghĩa vị trí theo mục tiêu.")
+                        raise Exception(f"Camera {cam_name} has no defined behavior in followCameraTask.")
 
-            # Cập nhật vị trí và hướng nhìn cho Cam_Main (độc lập với các camera chia màn hình)
+            # Update main camera to be slightly above and behind the target, looking at its head
             main_cam = self.camerasystem.get("Cam_Main", {}).get("camera")
             if main_cam:
-                main_cam.setPos(0, -self.cameraDistance, self.cameraHeight)
+                main_cam.setPos(0, -self.cameraDistance-10, self.cameraHeight+5)
                 main_cam.lookAt(target_pos.getX(), target_pos.getY(), target_pos.getZ()+1.5)
+                # angle_radians = heading * (pi / 180.0)
+                # # Tính toán vị trí camera nằm ở phía trước mặt nhân vật
+                # cam_x = target_pos.getX() - self.cameraDistance * sin(angle_radians)
+                # cam_y = target_pos.getY() + self.cameraDistance * cos(angle_radians)
+                # main_cam.setPos(cam_x, cam_y, self.cameraHeight)
+                # main_cam.lookAt(target_pos.getX(), target_pos.getY(), target_pos.getZ()+1.5)
         return task.cont
     
     def zoomtoBear(self):
+        """Toggle zoom to top-down view or back to main camera."""
         self.zoom =  not self.zoom
         if self.zoom:
-            self.camerasystem["Cam_Main"]["viewport"].setActive(1)
-            for cam_name in self.split_cameras:
-                self.camerasystem[cam_name]["viewport"].setActive(0)
-        else:
+            self.cameraPosition = self.camerasystem["Cam_TopDown"]["camera"].getPos()
             self.camerasystem["Cam_Main"]["viewport"].setActive(0)
             for cam_name in self.split_cameras:
                 self.camerasystem[cam_name]["viewport"].setActive(1)
+        else:
+            self.cameraPosition = self.camerasystem["Cam_Main"]["camera"].getPos()
+            self.camerasystem["Cam_Main"]["viewport"].setActive(1)
+            for cam_name in self.split_cameras:
+                self.camerasystem[cam_name]["viewport"].setActive(0)
+
+    def ListenCameraPosition(self,task):
+        """Listen to camera position changes and update the stored cameraPosition variable."""
+        if self.zoom:
+            self.cameraPosition = self.camerasystem["Cam_TopDown"]["camera"].getPos()
+        else:
+            self.cameraPosition = self.camerasystem["Cam_Main"]["camera"].getPos()
+        return task.cont
+
+
+    def getCameraPosition(self):
+        """Get the current position of the camera."""
+        return self.cameraPosition
+    
     def turnCamera(self, angle):
-        """Xoay camera quanh mục tiêu theo góc (độ)."""
+        """Turn the camera around the target by a specified angle (degrees)."""
         if self.target:
             heading = self.target.getH()
             new_heading = heading + angle
