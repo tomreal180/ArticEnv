5f048f9497bc3f5ccabb8f4be7599d854a64b655
Minh Duc Ha <tomreal180@gmail.com>
2026-07-22 12:13:25 +0200

    New: Training model completed

--- YoLo Model/train_model.py
+++ YoLo Model/train_model.py
@@ -5,20 +5,18 @@ from PIL import Image
 from IPython.display import display
 
 def main():
-    model = YOLO("yolov8n.pt")
+    model = YOLO("yolov8n.pt") # Load a pretrained YOLOv8n model
 
     print("Starting training...")
     results = model.train(
-        data='dataset.yaml',   # Trỏ tới file cấu hình vừa tạo
-        epochs=50,           # Số vòng lặp qua toàn bộ dataset. Bắt đầu với 50-100 vòng.
-        imgsz=640,             # Kích thước ảnh đầu vào (YOLO sẽ tự resize ảnh của bạn về 640x640)
-        batch=16,              # Số lượng ảnh đưa vào GPU cùng lúc. Giảm xuống 8 hoặc 4 nếu bị lỗi hết RAM.
-        name='arctic_bear_model', # Tên thư mục lưu kết quả
-        device='cpu'             # Sử dụng GPU đầu tiên. Đổi thành 'cpu' nếu máy không có card rời.
+        data='dataset.yaml',   # Point to configuration file for your dataset
+        epochs=50,           # Number of training epochs
+        imgsz=640,             # Input image size (YOLO will automatically resize your images to 640x640)
+        batch=16,              # Number of images sent to the GPU at once. Reduce to 8 or 4 if you encounter out-of-memory errors.
+        name='arctic_bear_model', # Name of the directory to save results
+        device='cpu'             # Train on CPU
     )
     print("Training completed. Results are saved in the directory 'runs/train/arctic_bear_model'.")
 
 if __name__ == "__main__":
     main()
-
-    
