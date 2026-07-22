7e5531d1b5eab26767502df4c8b3ddad4dcae770
Minh Duc Ha <tomreal180@gmail.com>
2026-07-22 13:00:30 +0200

    prettier

--- YoLo Model/test_model.py
+++ YoLo Model/test_model.py
@@ -1,11 +1,10 @@
 from ultralytics import YOLO
 
-# Tải bộ não AI bạn vừa train xong
+# Load a trained YOLOv8 model
 model = YOLO('runs/detect/arctic_bear_model/weights/best.pt')
 
-# Dự đoán trên một bức ảnh hoặc video mới
-# Tham số save=True sẽ tự động vẽ khung Bounding Box và lưu thành ảnh mới
+# Run inference on an image
 # for i in range(1, 6):
 #     results = model(f'test_image{i}.png', save=True, conf=0.5)
 results = model('test_real7.png', save=True, conf=0.5)
-print("Đã nhận diện xong, hãy vào thư mục 'runs/detect/predict' để xem kết quả!")
+print("Done detecting, please go to the directory 'runs/detect/predict' to view the results!")
